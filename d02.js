7d6185aae6956b8d2df594aad1f3cfecec8b474b
Marc A. Donis <marc@balloonary.io>
2023-04-01 19:05:18 +0200

    .

---
+++ d02.js
@@ -0,0 +1,26 @@
+const data = {
+	byline: 'Madrias Maderra',
+	headline: 'Donald Trump arrested for embarrassing humanity',
+	text: [
+		'In an unprecedented turn of events, former US President Donald Trump has been arrested today, April 1st, on charges of "embarrassing humanity" on a global scale. The decision follows a top-secret meeting of world leaders who determined that Trump\'s past actions and statements had crossed the line from political bluster to outright humiliation of the human race.',
+		"The arrest came as a shock to Trump's supporters, who had been eagerly awaiting his potential return to the political arena. Instead, they were greeted by the sight of the former President being led away in handcuffs, flanked by an elite team of international law enforcement agents from the newly established Global Embarrassment Prevention Unit (GEPU).",
+		'Trump\'s charges include a wide range of offenses, such as spreading misinformation, promoting divisiveness, and using social media to undermine the dignity of the human race. However, the most serious charge stems from his alleged role in the "covfefe" scandal, which authorities have deemed the "final straw" in their decision to take action.',
+		'While the legality of the arrest has come under scrutiny, the GEPU has defended its actions, stating that the protection of humanity\'s dignity is of paramount importance. "We cannot stand idly by as individuals tarnish the reputation of our species on a global stage," stated a spokesperson for the unit. "This is a matter of pride, not just for the leaders of the world, but for every single person who strives to make the world a better place."',
+		'In a show of solidarity, international social media platforms have agreed to permanently ban the former President from their sites. This ban is expected to extend to any future platforms Trump may attempt to create, as part of a sweeping effort to prevent further embarrassment to the human race.',
+		'Legal experts are divided on the potential outcome of the case, with some speculating that Trump could face a lifetime of mandatory etiquette lessons, while others argue that he may be sentenced to a lifetime of silence. The trial, anticipated to be one of the most highly publicized events of the century, will be presided over by a panel of esteemed judges, including Queen Elizabeth II, the Dalai Lama, and famed etiquette expert Miss Manners.',
+		'As the world awaits the outcome of this extraordinary case, some have embraced the arrest as a symbol of hope for humanity\'s future. "If we can recognize our mistakes and hold those responsible accountable, perhaps we can learn and grow from this experience," said one optimistic observer.',
+		'However, others have questioned the wisdom of the arrest, suggesting that it may only serve to further divide an already polarized world. "Embarrassment is subjective," argued a dissenting voice. "What one person finds humiliating, another may see as a source of pride."',
+		'Regardless of the outcome, one thing is certain: the world will be watching with bated breath as the saga unfolds.',
+		"<u>Disclaimer:</u> This article is a work of satire meant to celebrate April Fool's Day and is not to be taken as factual news",
+	],
+	image1: {
+		src: 'pic3_1.png',
+		alt: 'Mr. Trump sporting his new look',
+		style: 'width: 60%; padding-left: 20%',
+	},
+	image2: {
+		src: 'madc0w_miss_manners_judging_donald_trump_6bc94ed5-7317-4743-becb-e983cc7635ec.png',
+		alt: 'Miss Manners and Donald Trump enjoy an awkward moment together.',
+		style: 'width: 60%; padding-left: 20%',
+	},
+};
