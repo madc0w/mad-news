0a94bbb7f2817b56843baeb09d23625bbb324b5a
Marc A. Donis <marc@balloonary.io>
2023-04-16 19:16:04 +0200

    .

---
+++ d04.js
@@ -0,0 +1,30 @@
+const data = {
+	byline: 'Madrias Maderra',
+	headline:
+		'American tourists take walk in Luxembourg, discover country is very small',
+	text: [
+		'In a delightful twist of events, a group of American tourists, armed with their fanny packs and selfie-sticks, embarked on what they expected to be a leisurely stroll through the city of Luxembourg. Little did they know that they would stumble upon the astonishing realization that the entire country is, in fact, rather small.',
+
+		"The group, hailing from the United States - a country so vast that it could accommodate the entirety of Luxembourg approximately 3,500 times - were genuinely flabbergasted by the size of Europe's tiniest nation. Their reaction has since gone viral, with the hashtag #LittleLuxembourg trending on social media.",
+
+		"The tourists' epiphany struck when they inadvertently crossed the border into France while searching for a Starbucks, only to be greeted by a befuddled French border patrol officer who informed them they had, in fact, left Luxembourg behind.",
+
+		'"I just couldn\'t believe it," said Karen McTravel, leader of the group. "We walked for like, an hour, and suddenly we\'re in a whole different country. I thought we were still in Luxembourg City!"',
+
+		'Another member of the group, Chad Selfiestick, chimed in, "Yeah, where I come from, it takes hours just to drive across the state. Luxembourg\'s like a really cute, tiny version of America. So quaint!"',
+
+		'The locals have taken the incident in good humor, with many Luxembourgers sharing their own anecdotes of tourists underestimating the size of their country. ',
+
+		"<u>Disclaimer:</u> If it isn't obvious to you that this is fake news, then there is either something wrong with you, or with the world.",
+	],
+	image1: {
+		src: 'madc0w_American_tourist_in_Luxembourg_8fe58f79-bb43-48d5-8a63-616d8ea6bfcb.png',
+		alt: 'Chad gazes at France, from Luxembourg.',
+		style: 'width: 60%; padding-left: 20%',
+	},
+	image2: {
+		src: 'madc0w_Luxembourg_is_very_tiny_26a6d24b-6f5e-4fb2-9918-68c06bf19487.png',
+		alt: 'Even the buildings are finger-sized in Luxembourg.',
+		style: 'width: 60%; padding-left: 20%',
+	},
+};
