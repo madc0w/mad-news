aa2f07c73597b8940441b39790d6b0bd7edff6cc
Marc A. Donis <marc@balloonary.io>
2023-05-15 16:47:37 +0200

    .

---
+++ d06.js
@@ -0,0 +1,33 @@
+const data = {
+	byline: 'Madrias Maderra',
+	headline: 'Employee wins layoff lottery',
+	text: [
+		'In a bizarre twist of fate, a computer technician at a prestigious financial institution has struck gold in the form of a lucrative layoff package. Alan Lee, 39, a dedicated employee for over a decade, was reportedly ecstatic to learn he had been randomly selected to receive the generous offer, which includes a hefty severance package and additional benefits.',
+
+		"The layoff lottery, an unconventional incentive scheme devised by the company's HR department, was designed to ease the burden of downsizing while adding an element of excitement to the otherwise-dreaded redundancy process. Participation was optional, with employees voluntarily entering their names into a raffle, and Mr. Lee's name was the first to be drawn.",
+
+		'"I couldn\'t believe my luck," said Lee, grinning from ear to ear, "I\'ve been trying to get out of this soul-sucking job for years, and now they\'re actually paying me to leave! It\'s like winning the actual lottery, but with better odds."',
+
+		'The financial institution, which has requested anonymity due to the sensitive nature of the layoff lottery, defended the scheme as a creative and compassionate approach to a difficult situation. "We wanted to find a way to make the redundancy process more palatable for our employees," explained the head of HR, "and we believe the layoff lottery has added a touch of lightheartedness to an otherwise challenging time."',
+
+		"The layoff lottery has received mixed reviews from employees. Some have applauded the company's unconventional approach, while others have criticized it as insensitive and unprofessional. \"It's like they're making a game out of our livelihoods,\" said one disgruntled worker, who chose to remain anonymous. \"But I'd be lying if I said I didn't secretly hope to win.\"",
+
+		'Lee, however, has no such qualms. With his windfall in hand, he plans to take an extended vacation before pursuing his lifelong passion for landscape painting. "I always knew I was destined for something greater than fixing computers," he mused, "and now, thanks to the layoff lottery, I can finally unleash my inner Bob Ross."',
+
+		"As for the company, they have yet to confirm whether future rounds of the layoff lottery will be implemented. In the meantime, staff are said to be eagerly awaiting the next draw, with many hoping for the chance to follow in Lee's brushstrokes.",
+
+		'At press time, the financial institution has reported a sharp increase in lottery ticket purchases among employees, as well as a sudden surge in landscape paintings adorning the office walls.',
+
+		"<u>Disclaimer:</u> If it isn't obvious to you that this is fake news, then there is either something wrong with you, or with the world.",
+	],
+	image1: {
+		src: 'madc0w_chinese_man_working_at_a_computer_in_an_office_d560eed7-5562-42fb-ae1e-b591e76745b5.png',
+		alt: 'Lee, before winning the lottery.',
+		style: 'width: 60%; padding-left: 20%',
+	},
+	image2: {
+		src: 'madc0w_bad_oil_painting_of_office_interior_ec327a3d-4930-4ce3-9a67-2a6054febd3f.png',
+		alt: 'Some previous lottery winners left these souvenirs on the walls of their former office.',
+		style: 'width: 60%; padding-left: 20%',
+	},
+};
